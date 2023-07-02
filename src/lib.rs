7a9dcd9b76d6b5ba945868d80057700f013491ee
Aaron Bentley <aaron@aaronbentley.com>
2023-07-02 11:34:27 -0400

    Switch to quote macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, Data};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn no_op(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -9,22 +10,25 @@ pub fn no_op(_attr: TokenStream, item: TokenStream) -> TokenStream {
 #[proc_macro_derive(EvtPyclass)]
 pub fn evt_pyclass(item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
-    let mut outstr = format!("impl IntoPy<PyObject> for {}", ast.ident);
-    let Data::Enum(de) = ast.data else {
+    let Data::Enum(ref de) = ast.data else {
         panic!("Can only be used with enum.")
     };
-
-    outstr.push_str(r#" {
-    fn into_py(self, py: Python<'_>) -> PyObject {
-        match self {
-"#);
-    for variant in de.variants {
-    outstr.push_str(&format!(
-        "            {}::{}{{..}} => TryInto::<{}>::try_into(self).unwrap().into_py(py),\n", ast.ident, variant.ident, variant.ident));
+    let ident = ast.ident;
+    let mut arms = vec![];
+    for variant in &de.variants {
+        arms.push(variant.ident.clone())
     }
-    outstr.push_str(r#"        }
+    let x: TokenStream = quote! {
+        impl IntoPy<PyObject> for #ident {
+            fn into_py(self, py: Python<'_>) -> PyObject {
+                match self {
+                    #(#ident::#arms{..} => TryInto::<#arms>::try_into(self).unwrap().into_py(py),)*
+                }
+            }
+        }
     }
-}
-"#);
-    outstr.parse::<TokenStream>().expect("Generated unparseable shtuff")
+    .into();
+    eprintln!("{:?}", arms);
+    eprintln!("!! {:#?} !!", x);
+    x
 }
--- tests/01-parse.rs
+++ tests/01-parse.rs
@@ -1,14 +1,13 @@
+use enum_variant_type::EnumVariantType;
 use py_enum_macro::EvtPyclass;
 use pyo3::prelude::*;
-use enum_variant_type::EnumVariantType;
 
 #[derive(Debug, EvtPyclass, EnumVariantType)]
 enum Foo {
     #[evt(pyclass)]
-    Blue{num: i32},
+    Blue { num: i32 },
     #[evt(pyclass)]
-    Green{num: u32},
+    Green { num: u32 },
 }
 
-fn main() {
-}
+fn main() {}
