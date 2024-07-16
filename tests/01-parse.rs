b9f199a702b3db680cbb3510eede60c867fea6a9
Aaron Bentley <aaron@aaronbentley.com>
2024-07-16 01:17:20 -0400

    Implement FromPyObject in unit test.

--- tests/01-parse.rs
+++ tests/01-parse.rs
@@ -1,12 +1,13 @@
 use enum_variant_type::EnumVariantType;
-use pyo3_enum_variant_type::EvtPyclass;
 use pyo3::prelude::*;
+use pyo3_enum_variant_type::EvtPyclass;
+use pyo3::exceptions::PyTypeError;
 
 #[derive(Debug, EvtPyclass, EnumVariantType)]
 enum Foo {
-    #[evt(pyclass(set_all))]
+    #[evt(pyclass(set_all), derive(FromPyObject))]
     Blue { num: i32 },
-    #[evt(pyclass)]
+    #[evt(pyclass, derive(FromPyObject))]
     Green { num: u32 },
 }
 
@@ -15,4 +16,21 @@ fn enum_to_py(_py: Python, m: &PyModule) -> PyResult<()> {
     Foo::add_variant_structs(m)?;
     Ok(())
 }
-fn main() {}
+
+impl<'source> FromPyObject<'source> for Foo {
+    // Required method
+    fn extract(ob: &'source PyAny) -> PyResult<Self> {
+        if false {}
+        else if let Ok(x) = FromPyObject::extract(ob) {
+            return Ok(Blue::into(x));
+        }
+        else if let Ok(x) = FromPyObject::extract(ob) {
+            return Ok(Green::into(x));
+        }
+        Err(PyErr::new::<PyTypeError, _>("Cannot convert to Foo"))
+    }
+}
+
+fn main() {
+    let _x: Foo = Blue { num: 3 }.into();
+}
